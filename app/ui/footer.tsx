c94fb17fa5dc2e93892764b97ee3c46a453332d2
tu-nguyen-area <tunguyenarea@gmail.com>
2025-12-04 15:54:53 +0700

    Commit 3

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -1,14 +1,21 @@
 import Link from 'next/link';
 
 export default function Footer() {
+  const year = new Date();
+
   return (
   <>
 
-  <footer className="flex justify-center bg-gradient-to-r from-cyan-500 via-indigo-500 to-fuchsia-500 text-white p-6">
+  <footer className="flex justify-center bg-gradient-to-r from-cyan-500 via-indigo-500 to-fuchsia-500 text-white p-6 text-sm md:text-base">
     <section>
-      <p>Privacy Polity | Terms of Use</p>
-      <p>A product from <Link href="https://tu-nguyen.org/" target="_blank">TN ADVANCEMENT</Link></p>
-      <p>&#169; 2025 TN Tourism <span className="hidden md:inline-block">Project</span>. All rights reserved.</p>
+      <p>Privacy Policy | Terms of Use</p>
+      <p>Powered by <Link
+        href="https://tnadvancement.tn-technology.com/"
+        target="_blank"
+      >
+        TN ADVANCEMENT</Link>
+      </p>
+      <p>&#169; {year.getFullYear()} TN Tourism Project. All rights reserved</p>
     </section>
   </footer>
 
